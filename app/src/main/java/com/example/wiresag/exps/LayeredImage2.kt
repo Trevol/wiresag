bd096808ef5079c92a4e67767c5df61863efcafd
trevol <trvo@smart-dn.ru>
2023-01-03 12:04:44 +0300

    improve pan and zoom WIP. Wipe TransformState

--- app/src/main/java/com/example/wiresag/exps/LayeredImage2.kt
+++ app/src/main/java/com/example/wiresag/exps/LayeredImage2.kt
@@ -1,12 +1,10 @@
 package com.example.wiresag.exps
 
 import androidx.compose.foundation.Canvas
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.ImageBitmap
 import androidx.compose.ui.graphics.drawscope.DrawScope
-import androidx.compose.ui.layout.onGloballyPositioned
-import androidx.compose.ui.unit.IntSize
 
 @Composable
 fun LayeredImage2(
@@ -18,14 +16,11 @@ fun LayeredImage2(
     onLongClick: RemappedClick2 = NoRemappedClick2,
     onLayerDraw: DrawScope.() -> Unit
 ) {
-    val state = remember { TransformState() }
     Canvas(
         modifier = modifier
-            //.onSizeChanged { state.size = it }
-            .onGloballyPositioned { state.size = it.size }
             .transformableAndClickable2(
                 transform,
-                onGesture = { onTransform(state.toTransformation(transform, it)) },
+                onGesture = { onTransform(transform.applyGesture(it)) },
                 onClick = onClick,
                 onLongClick = onLongClick
             )
@@ -35,57 +30,12 @@ fun LayeredImage2(
     }
 }
 
-
-private class TransformState {
-    var size by mutableStateOf(IntSize.Zero)
-
-    fun toTransformation(
-        prevTransform: TransformParameters,
-        gesture: PanZoomGesture
-    ): TransformParameters {
-        if (gesture.isReleaseEvent) {
-            return prevTransform
-        }
-        return TransformParameters(
-            translation = prevTransform.translation + gesture.pan + gesture.centroid * (1 - gesture.zoom),
-            scale = prevTransform.scale * gesture.zoom,
+private fun TransformParameters.applyGesture(gesture: PanZoomGesture) =
+    if (gesture.isReleaseEvent) {
+        this
+    } else
+        TransformParameters(
+            translation = translation + gesture.pan + gesture.centroid * (1 - gesture.zoom),
+            scale = scale * gesture.zoom,
             gesture = gesture
         )
-    }
-
-    fun oldStuff() {
-        /*
-        var gestureEvent by rememberMutableStateOf(null as GestureEvent?)
-        var size = remember { IntSize.Zero }
-        var prevOrigin = remember { TransformOrigin.Center }
-        val transformParameters by rememberDerivedStateOf {
-
-            val gestureRequest = gestureEvent
-            val transformParams = if (gestureRequest == null) {
-                transform
-            } else {
-                val origin = TransformOrigin(0f, 0f)
-                if (transformationParams.centroidSize == 0f ||
-                    transformationParams.centroid == Offset.Unspecified ||
-                    transformationParams.centroid == Offset.Infinite ||
-                    size == IntSize.Zero
-                ) {
-                    prevOrigin
-                } else {
-                    prevOrigin = TransformOrigin(
-                        transformationParams.centroid.x / size.width,
-                        transformationParams.centroid.y / size.height
-                    )
-                    prevOrigin
-                }
-                TransformParameters(
-                    translation = gestureRequest.pan,
-                    scale = gestureRequest.zoom,
-                    transformOrigin = origin
-                )
-            }
-            transformParams
-        }
-        */
-    }
-}
